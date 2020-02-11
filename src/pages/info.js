06c3c3537de144a9a324c3e57151bcea688f077c
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2020-02-11 05:54:15 -0300

    basic info, only works in expo for now

---
+++ src/pages/info.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import {Image, StyleSheet, Text, View} from 'react-native';
+var{vw, vh, vmin, vmax} = require('react-native-expo-viewport-units');
+
+const cityscape = require('../../res/montgomery.jpeg');
+const person = require('../../res/Rosaparks.jpg');
+
+
+export default function Info(){
+  
+
+ return (
+   <View>
+    <Image
+      source={cityscape}
+      style={styles.cover}
+    />
+    <Image
+      source={person}
+      style={styles.person}
+    />
+
+   <Text style={styles.info}>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+   </Text>
+   </View>
+ )
+
+}
+
+const styles = StyleSheet.create({
+  cover: {
+    height: '40%',
+    width: '100%'
+  },
+
+  person: {
+    position: 'relative',
+    top: -vh(10),
+
+    marginLeft:   'auto',
+    marginRight:  'auto',
+
+    borderColor: '#FFF',
+    borderWidth: 5,
+    borderRadius: 100,
+    
+    width: vh(20),
+    height: vh(20)
+  },
+
+  info: {
+    paddingHorizontal: 10,
+    fontSize: 16
+  },
+
+})
+
