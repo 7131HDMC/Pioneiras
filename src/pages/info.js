56b2b4ea4d10d60dd4ee8aad93c09fa41a970551
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2020-02-20 00:32:02 -0300

    extending modularity to text

--- src/pages/info.js
+++ src/pages/info.js
@@ -27,7 +27,7 @@ export default function Info({ navigation }){
     />
 
    <Text style={styles.info}>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    {resources.text}
    </Text>
    </View>
  )
