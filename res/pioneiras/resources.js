748bb21928cd6cdb851e235275c3c10da03f2800
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2020-03-02 16:18:19 -0300

    beggining to implement additional info

--- res/pioneiras/resources.js
+++ res/pioneiras/resources.js
@@ -1,5 +1,22 @@
 export default res = {
   root: {
+    'amazonas':{
+      name: 'Amazonas',
+      author: ['Júlia Braga Marques Pereira'],
+      avatar: require('./amazonas/avatar.jpg'),
+      cover: require('./amazonas/cover.jpeg'),
+      flavortext: "Segundo a lenda das Icabiabas, também conhecidas por Amazonas foram uma Tribo lendária, formada exclusivamente por mulheres guerreiras. Esse mito faz parte da cultura brasileira. Eram musculosas, de cabelos longos e soltos ao vento. Elas mutilavam o seio direito, para poder ter facilidade ao manusear arco, lança ao atacar os inimigos, assim também para se defender e caçar. Viviam ás margens do Rio Amazonas.",
+      text: "Elas tinham como Deusa a Lua, que na sua concepção as protegiam. Todo dia de lua cheia era comemorado com rituais, que ocorriam com a chamada de índios de outras tribos, momento em que pediam autorização à Divindade, para que pudessem ter relações sexuais. Elas faziam um amuleto chamado muiraquitã, com formato de rãs, peixes, tartarugas e felino, que era bastante resistente. Entediam que isso as protegiam e davam aos índios com os quais se relacionavam. Na década de 1920, a lenda sobre as Amazonas teve   outra versão, na ótica do jornalista Heitor Moniz. Em seus escritos as guerreiras ficam além da foz do rio Nhamundá, à margem esquerda do rio Amazonas. Além de guerrear, elas eram mães, mas preservava apenas as filhas, e os seus filhos homens davam ao pai para cuidar. Tinham relações sexuais com os índios somente para a conservação da espécie. Diz a lenda que em 1539 o navegador Francisco Orellana percorreu o norte do Brasil e   deparou com as amazonas, que atacaram sua frota. Em 1778, o naturalista francês Charles de La Condamine, relataou à Real Academia de Ciências de Paris, ter encontrado indícios da existencia das guerreiras icambiabas. Ele teria percorrido a bacia amazônica, seguindo o mesmo caminho do navegador espanhol Orellana. Em pleno Sec. XIX, o viajante inglês Southey, relatou a possibilidade de que as guerreiras não fossem só lenda, mas que realmente tivessem existido. Pode-se concluir que as Amazonas existiram e ainda vivem no imaginário de cronistas e historiadores.",
+      extra: [
+        [
+          "A Lenda das Amazonas",
+          "As Icamiabas - Romance Das Mulheres Guerreiras Sem Marido e Seus Muiraquitãs",
+          "Livro: As Amazonas",
+          "Bernardo E O Enigma Das Amazonas"
+        ]
+      ]
+
+    },
     'rosa-parks':{
       name: "Rosa Parks",
       avatar: require('./rosa-parks/avatar.jpg'),
