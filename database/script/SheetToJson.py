36178f3180b0155d6f993db045d22136d69f691a
Hari Dasa Fiuza de Moura <haridasafiuza@gmail.com>
2020-05-10 13:37:27 -0300

    read current database

--- database/script/SheetToJson.py
+++ database/script/SheetToJson.py
@@ -7,15 +7,31 @@ import json
 class SheetToJson:
   data = 0
   headers = 0
-  __init__(self):
-    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-    file_name = "../../../UpdatePioneers/pioneiras-key.json"
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(file_name, scope)
-    gc = gspread.authorize(credentials)
-    wks = gc.open('Pioneiras').sheet1
-    self.data = wks.get_all_values()
-    headers = data.pop(0)
+  pioneers = 0
+  def __init__(self):
+	scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+	file_name = "/home/monkey/Desktop/pioneiras/UpdatePioneers/pioneiras-key.json"
+	credentials = ServiceAccountCredentials.from_json_keyfile_name(file_name, scope)
+	gc = gspread.authorize(credentials)
+	wks = gc.open('Pioneiras').sheet1
+	self.data = wks.get_all_values()
+	headers = data.pop(0)
 
 
-  def changeHeaders(self):
+  def makeJson(self):
   	df = pd.DataFrame(data, columns=headers)
+
+  def getData():
+  	with open('../database_pioneiras.js') as arq:
+  	  arq_str = arq.read()	
+      
+      #delete all the isn't that json data  
+      #tudo antes de {
+      key = arq_str.find('{')
+      rest = arq_str[:key]
+      arq_str = arq_str.replace(rest,'')
+      #tudo apos ;, incluso
+      key = arq_str.find(';')
+      rest = arq_str[key:]
+      arq_str = arq_str.replace(rest,'')
+      #retirrar o reuire -> make a func
