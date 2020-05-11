a25691b5c9f66377e1ff447527d931af9cd2f009
Hari Dasa Fiuza de Moura <haridasafiuza@gmail.com>
2020-05-11 16:42:35 -0300

    current database turn json

--- database/script/SheetToJson.py
+++ database/script/SheetToJson.py
@@ -3,35 +3,35 @@ from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import requests
 import json
+from database import CurrentDataBase 
+
 
 class SheetToJson:
   data = 0
   headers = 0
   pioneers = 0
+  """
   def __init__(self):
+    #url's for get Pioneras's sheet
 	scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+	#directory of API key file
 	file_name = "/home/monkey/Desktop/pioneiras/UpdatePioneers/pioneiras-key.json"
-	credentials = ServiceAccountCredentials.from_json_keyfile_name(file_name, scope)
-	gc = gspread.authorize(credentials)
-	wks = gc.open('Pioneiras').sheet1
-	self.data = wks.get_all_values()
-	headers = data.pop(0)
-
+	#directory of current pioneers data
+	
+	#getting remote pioneers data from google sheets 
+	self.getSheet(file_name, scope)
+	#removing the headers
+	headers = self.data.pop(0)
+	#getting the current data of pioneers
+	
+  """
+  def getSheet(file_name, scope):
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(file_name, scope)    
+    gc = gspread.authorize(credentials)
+    wks = gc.open('Pioneiras').sheet1
+    self.data = wks.get_all_values()
 
   def makeJson(self):
-  	df = pd.DataFrame(data, columns=headers)
+    df = pd.DataFrame(data, columns=headers)
 
-  def getData():
-  	with open('../database_pioneiras.js') as arq:
-  	  arq_str = arq.read()	
-      
-      #delete all the isn't that json data  
-      #tudo antes de {
-      key = arq_str.find('{')
-      rest = arq_str[:key]
-      arq_str = arq_str.replace(rest,'')
-      #tudo apos ;, incluso
-      key = arq_str.find(';')
-      rest = arq_str[key:]
-      arq_str = arq_str.replace(rest,'')
-      #retirrar o reuire -> make a func
+
---
+++ database/script/database.py
@@ -0,0 +1,39 @@
+
+
+class CurrentDataBase:
+  data_file = '../database_pioneiras.js'
+  def getData(self,file_name):
+    with open(file_name) as arq:
+      arq_str = arq.read()	#return file as string
+      arq_str = self.removeJS(arq_str)
+      pioneers = json.loads(arq_str)
+      return pioneers 
+  """
+  file is a string
+  """  
+  def removeJS(self,file):
+    #remove all before the first '{'    
+    key = file.find('{')
+    rest = file[:key]
+    file = file.replace(rest,' ')
+    #remove all after ';', with it
+    key = file.find(';')
+    rest = file[key:]
+    file = file.replace(rest,' ')
+    file = file.replace("require(\"","\"<")      
+    file = file.replace("\"),\n", ">\",\n")
+    return file
+
+  """
+  file is a string 
+  """
+  def putJS(self,file):
+    file = file.replace("\"<","require(\"")     
+    file = file.replace( ">\",\n" ,"\"),\n")
+    key = file.find('{')
+    rest = file[:key]
+    js_command = 'var res = '
+    file = file.replace(' ', js_command, 1)
+
+    file = file + '; \n export default res;'
+    return file
