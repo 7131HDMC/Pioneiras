a19654fcd11f601a6a9f683b5cd0388f0ad67168
Hari Dasa Fiuza de Moura <haridasafiuza@gmail.com>
2020-05-23 18:55:08 -0300

    sheet to json finished

--- database/script/SheetToJson.py
+++ database/script/SheetToJson.py
@@ -3,17 +3,11 @@ from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import requests
 import json
+
 from currentData import CurrentData 
+from consumeDriveImage import DriveImage
 
 class SheetToJson:
-  data = 0
-  headers = 0
-
-  pioneers = 0
-  df = 0
-
-  century = 0
-  name = 0
   def __init__(self):
     #url's for get Pioneras's sheet
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
@@ -25,8 +19,8 @@ class SheetToJson:
     #removing the headers
     self.headers = self.data.pop(0)
     #getting the current data of pioneers
-    current = CurrentData()
-    self.pioneers = current.getData()
+    self.current = CurrentData()
+    self.pioneers = self.current.getData()
     self.df = pd.DataFrame(self.data, columns=self.headers)
 
   def getSheet(self,file_name, scope):
@@ -34,9 +28,10 @@ class SheetToJson:
     gc = gspread.authorize(credentials)
     wks = gc.open('Pioneiras').sheet1
     self.data = wks.get_all_values()
-
-  #def makeJson(self):
     
+  def writeJson(self, file_str):
+    with open(self.current.data_file, "w") as file:
+      file.write(file_str)
   def loop(self): 
     pioneer_js = {"extra":[]}
     headers = len(self.df.keys())
@@ -47,16 +42,32 @@ class SheetToJson:
         if key == 9 or key == 12 or key == 15 or key == 10 or key == 13 or key == 16 or key == 17:
           continue
         self.linkValues(key=key,pioneer=pioneer,js=pioneer_js)
+      self.pioneers[self.century][self.name] = pioneer_js
+    str_pioneers = str(self.pioneers)
+    str_pioneers = self.current.putJS(str_pioneers)
+    self.writeJson(str_pioneers)
     #self.makeJson(pioneer_js)
     print(pioneer_js)
+    #print("\n Putting JS \n")
+
 
   def linkValues(self,key, pioneer, js):
     if key == 1:
       self.century = self.getDataTime(pioneer[key])
     elif key == 2:
-      pass
+      url = pioneer[key]
+      name = pioneer[5]
+      type_name = "avatar.png"
+      image = DriveImage(url, name, type_name)
+      js["avatar"] = image.main()
+
     elif key == 3:
-      pass
+      url = pioneer[key]
+      type_name = "cover.png"
+      name = pioneer[5]
+      image = DriveImage(url, name, type_name)
+      js["cover"] = image.main()
+
     elif key == 5 :
       self.name = pioneer[key]
       js["name"] = self.name
@@ -74,7 +85,7 @@ class SheetToJson:
         text_extra_type = pioneer[(k+1)]
         text_extra = pioneer[(k+2)]
         js["extra"].append({"type": text_extra_type,  "content" : text_extra})
-    print(js)
+    #print(js)
       
   def getDataTime(self,datatime): 
     if '﻿Raiz (< Séc.XVI  até XVII)'== datatime: 
--- database/script/consumeDriveImage.py
+++ database/script/consumeDriveImage.py
@@ -1,29 +1,83 @@
 import requests
+import os
+import io
+import pickle
+from googleapiclient.http import MediaIoBaseDownload 
+import google_auth_oauthlib.flow 
+from google.auth.transport.requests import Request 
+from google_auth_oauthlib.flow import InstalledAppFlow 
+from googleapiclient.discovery import build 
 
-class consumeDriveImage:
-    link = ""
-  def __init__(self, url):
+
+
+class DriveImage:
+
+  def __init__(self, url, name, type_name):
+    SCOPES = "https://www.googleapis.com/auth/drive.readonly" 
+    CLIENT_SECRET_FILE = "/home/monkey/Desktop/pioneiras/UpdatePioneers/credentials.json"   
+    credentials = self.auth(client_secret_filename=CLIENT_SECRET_FILE, scopes=SCOPES)
     self.link = url
+    self.name = name
+    self.type_name = type_name
+    self.drive = build('drive', 'v3', credentials=credentials) 
 
-  def getToken(self,response):
-    for key, value in response.cookies.items():
-      if key.startswith('download_warning')
-        return value
-    return none
+  def auth(self, client_secret_filename, scopes):
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('/home/monkey/Desktop/pioneiras/UpdatePioneers/token.pickle'):
+      with open('/home/monkey/Desktop/pioneiras/UpdatePioneers/token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+      if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+      else:
+        flow = InstalledAppFlow.from_client_secrets_file(client_secret_filename, scopes)
+        creds = flow.run_local_server()
+      # Save the credentials for the next run
+      with open('/home/monkey/Desktop/pioneiras/UpdatePioneers/token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+    return creds
+  def main(self):
+    id = self.getID()
+    return self.getImage(id)
+     
+
   def getImage(self, id):
-    session = requests.Session()
-    response = session.get(url, params = { 'id' : id }, stream = True)
-    token = self.getToken(response)
-    if token:
-      params = { 'id' : id, 'confirm' : token }
-      response = session.get(self.link, params = params, stream = True)
+    request = self.drive.files().get_media(fileId=id) 
+    file = io.BytesIO() 
+    downloader = MediaIoBaseDownload(file, request) 
+    done = False 
+    while done is False: 
+      status, done = downloader.next_chunk() 
 
-  def saveImg(self, reponse, destination):
-    CHUNK_SIZE = 32768
-    with open(destination, "wb") as file:
-      for chunk in  response.iter_content(CHUNK_SIZE):
-        if chunk:
-          file.write(chunk)
+    head = self.getDestination()
+    return self.saveImg(file, head)
+    
+
+  def saveImg(self, file, destination):
+    file.seek(0)
+    with open(destination,"wb") as f: 
+      for chunk in file: 
+        if chunk: 
+          f.write(chunk) 
+      return destination
+
   def getDestination(self):
-    pass
+    dir_pioneer =  "../../assets/pioneiras/"+ self.name
+    if ~os.path.exists(dir_pioneer):
+      try:
+        os.mkdir(dir_pioneer)
+      except OSError:
+        print("OSError: error to create directory {} ".format(dir_pioneer))
+    
+    dir_pioneer += "/" + self.type_name 
+    return dir_pioneer
+
+  def getID(self):
+    equal = self.link.find('=')
+    id = self.link[equal+1:]
+    return id
 
